4ef496042fa9c4ec0805b13e9fdc031dc8c4f3d3
junwoo45 <sinyoorang@gmail.com>
2020-05-18 18:24:22 +0900

    docs: approach2 - service

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -22,6 +22,8 @@ export class CourseComponent implements OnInit {
   }
 
   ngOnInit() {
+    // 서비스로 빼내는 방법 말고, 더 잘할 수 없을까?
+    // 여전히 If-else문이 scaleable하지 않다.(표현이 이게 맞나?)
     this.color = this.courseService.getColor(this.course.type);
   }
 
